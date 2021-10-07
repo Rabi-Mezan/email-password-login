ad06a997a9862fcfd2ab7df22d25d71b96c63ee3
Rabi-Mezan <ravimezan@gmail.com>
2021-10-07 18:44:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, updateProfile } from 'firebase/auth'
+import { useState } from 'react';
 import './App.css';
+import initAuth from './Firebase/Init';
+
+initAuth()
 
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLogIn, setIsLogIn] = useState(false)
+
+  const auth = getAuth();
+
+  const handleName = e => {
+    setName(e.target.value)
+  }
+  const handleEmail = e => {
+    setEmail(e.target.value)
+  }
+  const handlePassword = e => {
+    setPassword(e.target.value)
+  }
+
+  const toggleCheckBox = e => {
+    setIsLogIn(e.target.checked)
+  }
+
+  const handleRegister = e => {
+    e.preventDefault();
+    if (password.length < 6) {
+      setError('password must be 6 character');
+      return;
+    }
+    isLogIn ? userLogin(email, password) : createUser(email, password)
+
+  }
+
+  const userLogin = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user
+        console.log(user)
+        setError("")
+      })
+      .catch(error => {
+        setError(error.message)
+      })
+  }
+
+  const createUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        setError('');
+        verifyEMail();
+        setUsername();
+
+      })
+      .catch(error => {
+        setError(error.message)
+      })
+  }
+
+  const verifyEMail = () => {
+    sendEmailVerification(auth.currentUser)
+      .then(result => {
+
+      })
+  }
+
+  const handleResetPassword = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(result => {
+
+      })
+  }
+  const setUsername = () => {
+    updateProfile(auth.currentUser, { displayName: name })
+      .then(result => { })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Please {isLogIn ? "Login" : "Register"}</h3>
+      <form onSubmit={handleRegister} className='reg-form'>
+        {!isLogIn && <div className='field'>
+          <label htmlFor="name">Name</label>
+          <input onBlur={handleName} type="text" name='name' required />
+        </div>}
+        <br />
+        <div className='field'>
+          <label htmlFor="email">Email</label>
+          <input onBlur={handleEmail} type="text" name='email' required />
+        </div>
+        <br />
+        <div className='field'>
+          <label htmlFor="password">Password</label>
+          <input onBlur={handlePassword} type="password" name='password' required />
+        </div>
+        <br />
+        <input onChange={toggleCheckBox} type="checkbox" name="Existing User" id="" />
+        <label htmlFor="checkbox">Existing User ?</label>
+        <button type='submit'>{isLogIn ? "Login" : "Register"} </button>
+        <button onClick={handleResetPassword}>Reset Password</button>
+
+      </form>
+
+      <p>{error}</p>
+
     </div>
   );
 }
---
+++ src/Firebase/config.js
@@ -0,0 +1,12 @@
+// import firebaseConfig from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqd1Qcea8sYOZjKKKawB1cnUX6gvM0Dc0",
+    authDomain: "email-pass-login-d8366.firebaseapp.com",
+    projectId: "email-pass-login-d8366",
+    storageBucket: "email-pass-login-d8366.appspot.com",
+    messagingSenderId: "162499042343",
+    appId: "1:162499042343:web:01a5e954982f55dfdb4202"
+};
+
+export default firebaseConfig
